6453a57d201b3cde9f6dc9d03da2b1a53feb371d
Abdel Raoof <olakara@gmail.com>
2024-10-19 10:14:26 +0400

    pointers initial

---
+++ basic/pointers/point.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var actualStringVariable string
+	var addressOfStringVariable *string
+	fmt.Println("actualStringVariable: ", actualStringVariable)
+	fmt.Println("addressOfStringVariable: ", addressOfStringVariable)
+}
