fb947b4eaab2f2987ece3292c757bcfb65b778f7
Abdel Raoof <olakara@gmail.com>
2024-10-19 10:14:13 +0400

    array summation

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,5 +1,9 @@
 package arrays
 
-func Sum() int {
-	return 40
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -5,11 +5,23 @@ import "testing"
 func TestSum(t *testing.T) {
 
 	t.Run("should return sum of integers", func(b *testing.T) {
-		sum := Sum()
-		expected := 40
+		numbers := [5]int{1, 2, 3, 4, 5}
+
+		sum := Sum(numbers[:])
+		expected := 15
 
 		if sum != expected {
 			t.Errorf("Expected to get %d, but got %d", expected, sum)
 		}
+	})
+
+	t.Run("should return 0 when there is no numbers passed", func(t *testing.T) {
+		numbers := []int{}
+		sum := Sum(numbers)
+		expected := 0
+		if sum != expected {
+			t.Errorf("Expected to get 0, but got %d", sum)
+		}
+
 	})
 }
