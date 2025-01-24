941ed28f9d38a6b02f7c9647e10defcde50549e8
Abdel Raoof <olakara@gmail.com>
2025-01-24 14:47:47 +0400

    random number generation

---
+++ random/cmd/main.go
@@ -0,0 +1,12 @@
+﻿package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("A random number: ", rand.Int())
+	fmt.Println("Another random number: ", rand.Intn(10))
+	fmt.Println("Yet another random number: ", rand.Uint32())
+}
