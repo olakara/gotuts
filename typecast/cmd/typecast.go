0d13e029dd8b350f687d5ce73e5578f688ae7e42
Abdel Raoof <olakara@gmail.com>
2025-01-25 17:21:56 +0400

    typecast to learn type conversion

---
+++ typecast/cmd/typecast.go
@@ -0,0 +1,13 @@
+﻿package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var x, y int = 4, 5
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var round float64 = math.Round(f*100) / 100
+	fmt.Println(x, y, f, round)
+}
---
+++ typecast/go.mod
@@ -0,0 +1,3 @@
+module abraol.com/typecast
+
+go 1.23.0
