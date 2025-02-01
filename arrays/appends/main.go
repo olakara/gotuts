cda85fcf1c2a11b1c46c6580e0d164c5b93be577
Abdel Raoof <olakara@gmail.com>
2025-02-01 16:15:10 +0400

    merging of two slice to one..

---
+++ arrays/appends/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var names []string
+	names = append(names, "John")
+	names = append(names, "Doe", "Smith")
+	fmt.Println(names)
+	newNames := names
+	newNames[0] = "Sara"
+	fmt.Println(newNames)
+
+	// merging of two slice to one
+	another := append(names, newNames...)
+	fmt.Println(another)
+}
