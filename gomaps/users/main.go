ae803f0537a164142e4b26533e4435f53c1340ca
Abdel Raoof <olakara@gmail.com>
2025-02-01 22:37:45 +0400

    user struct with map

---
+++ gomaps/users/main.go
@@ -0,0 +1,33 @@
+﻿package main
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID   uuid.UUID
+	Name string
+	Age  int
+}
+
+func main() {
+	users := map[uuid.UUID]User{}
+	id := uuid.New()
+
+	users[id] = User{
+		ID:   id,
+		Name: "John Doe",
+		Age:  30,
+	}
+
+	id = uuid.New()
+
+	users[id] = User{
+		ID:   id,
+		Name: "Sara Smith",
+		Age:  26,
+	}
+
+	fmt.Println(users)
+}
