47608f6abe91daa12a815dbb5363cc5a4f073ee7
Abdel Raoof <olakara@gmail.com>
2025-02-01 14:30:33 +0400

    create an array of 4 strings

---
+++ arrays/first/first.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Array of four strings
+	names := [4]string{"John", "Paul", "George", "Ringo"}
+	fmt.Printf("%#v\n", names)
+	fmt.Println(names[2])
+}
