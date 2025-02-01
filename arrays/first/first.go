97885e00725bf003005ce9912013713e72652575
Abdel Raoof <olakara@gmail.com>
2025-02-01 14:32:54 +0400

    create a slice of 4 strings

--- arrays/first/first.go
+++ arrays/first/first.go
@@ -8,4 +8,9 @@ func main() {
 	names := [4]string{"John", "Paul", "George", "Ringo"}
 	fmt.Printf("%#v\n", names)
 	fmt.Println(names[2])
+
+	// Slice of strings
+	sliceNames := []string{"John", "Paul", "George", "Ringo"}
+	fmt.Printf("%#v\n", sliceNames)
+	fmt.Println(sliceNames[2])
 }
