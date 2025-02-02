24658270e878c75cc865f714b46eb817f7ee4d58
Abdel Raoof <olakara@gmail.com>
2025-02-02 13:03:44 +0400

    functions with multiple return values

---
+++ functions/info/main.go
@@ -0,0 +1,19 @@
+﻿package main
+
+import "fmt"
+
+func getInfo(names []string) (string, int, int) {
+	formattedString := fmt.Sprintf("The names are: %v", names)
+	length := len(names)
+	capacity := cap(names)
+	return formattedString, length, capacity
+}
+
+func main() {
+	names := []string{"John", "Paul", "George", "Ringo"}
+
+	formattedString, length, capacity := getInfo(names)
+	fmt.Println(formattedString)
+	fmt.Println("Length:", length)
+	fmt.Println("Capacity:", capacity)
+}
