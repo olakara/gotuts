9ae5b3bf1c6691cd6b523b3a02116c0c7660d5b9
Abdel Raoof <olakara@gmail.com>
2025-02-03 17:06:52 +0400

    making my first struct

---
+++ structures/first/main.go
@@ -0,0 +1,28 @@
+﻿package main
+
+import "fmt"
+
+type User struct {
+	ID   int
+	Name string
+	Age  int
+}
+
+func main() {
+
+	user := User{
+		ID:   1,
+		Name: "John",
+		Age:  25,
+	}
+
+	user2 := User{
+		ID:   2,
+		Name: "Jane",
+		Age:  30,
+	}
+
+	users := []User{user, user2}
+
+	fmt.Println(users)
+}
