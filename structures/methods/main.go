8b4b8e66ee6234f1447e66c57ea5d111bd41468b
Abdel Raoof <olakara@gmail.com>
2025-02-03 17:26:07 +0400

    methods for a struct

---
+++ structures/methods/main.go
@@ -0,0 +1,24 @@
+﻿package main
+
+import "fmt"
+
+type User struct {
+	ID   int
+	Name string
+	Age  int
+}
+
+func (u User) GetID() int {
+	return u.ID
+}
+
+func (u User) String() string {
+	return fmt.Sprintf("ID: %d, Name: %s, Age: %d", u.ID, u.Name, u.Age)
+}
+
+func main() {
+	u := User{ID: 1, Name: "John Doe", Age: 25}
+
+	fmt.Println(u.GetID())
+	fmt.Println(u.String())
+}
