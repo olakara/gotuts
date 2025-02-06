fd78cfc16c1133f877355409f63772a9fc02861b
Abdel Raoof <olakara@gmail.com>
2025-02-06 20:32:07 +0400

    basic testing

---
+++ tests/basic/demo.go
@@ -0,0 +1,5 @@
+package main
+
+func isOdd(i int) bool {
+	return i%2 != 0
+}
---
+++ tests/basic/demo_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func Test_IsOdd_ReturnsTrueWhenOdd(t *testing.T) {
+	want := true
+	got := isOdd(3)
+	if got != want {
+		t.Fatalf("isOdd(3): got %v want %v", got, want)
+	}
+}
+
+func Test_IsOdd_ReturnsFalseWhenEven(t *testing.T) {
+	want := false
+	got := isOdd(4)
+	if got != want {
+		t.Fatalf("isOdd(4): got %v want %v", got, want)
+	}
+}
