aea5cbe5634e03443a5fe23e1d20eead18dfd76b
Abdel Raoof <olakara@gmail.com>
2025-02-08 21:16:42 +0400

    get the type of musician

--- interface/main.go
+++ interface/main.go
@@ -1,8 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 	musician := Musician{"Mozart"}
 	PerformAtVenu(musician)
+	fmt.Printf("type %T\n", musician)
 
 	poet := Poet{"William Wordsworth"}
 	PerformAtVenu(poet)
