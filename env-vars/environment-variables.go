64cb18ec48a3630de11466845723019dac8f8b85
Abdel Raoof <olakara@gmail.com>
2025-02-11 22:53:54 +0400

    reading data from yaml file

--- env-vars/environment-variables.go
+++ env-vars/environment-variables.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/spf13/viper"
 	"os"
 )
 
@@ -12,6 +13,24 @@ func main() {
 	fmt.Println("KEY:", os.Getenv("KEY"))
 	os.Setenv("KEY", "VALUE")
 	fmt.Println("KEY:", os.Getenv("KEY"))
-	os.Unsetenv("KEY")
-	fmt.Println("KEY:", os.Getenv("KEY"))
+
+	viper.SetDefault("ViperKey", "ViperValue")
+	fmt.Println("ViperKey:", viper.Get("ViperKey"))
+	viper.Set("ViperKey", "ViperValue2")
+	fmt.Println("Updated ViperKey:", viper.Get("ViperKey"))
+
+	viper.SetConfigFile("config.yaml")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("Error reading config file", err)
+	}
+
+	fmt.Println("Project from config file:", viper.Get("project"))
+	fmt.Println("Version from config file:", viper.Get("version"))
+	types := viper.GetStringSlice("types")
+	for _, value := range types {
+		fmt.Println("Type:", value)
+	}
 }
