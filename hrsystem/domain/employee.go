425be62932f1c7d12275d2d4d8026566fba2044a
Abdel Raoof <olakara@gmail.com>
2025-02-17 20:46:53 +0400

    trying tdd on hr system

---
+++ hrsystem/domain/employee.go
@@ -0,0 +1,25 @@
+﻿package domain
+
+import "errors"
+
+type Employee struct {
+	firstName string
+	lastName  string
+}
+
+func NewEmployee(firstName, lastName string) (*Employee, error) {
+	if firstName == "" {
+		return nil, errors.New("first name is required")
+	}
+	if lastName == "" {
+		return nil, errors.New("last name is required")
+	}
+	return &Employee{
+		firstName: firstName,
+		lastName:  lastName,
+	}, nil
+}
+
+func (e *Employee) GetFullName() string {
+	return e.firstName + " " + e.lastName
+}
---
+++ hrsystem/domain/employee_test.go
@@ -0,0 +1,52 @@
+﻿package domain
+
+import (
+	"testing"
+)
+
+func TestNewEmployee_WhenAllDataIsProvided_CreateEmployee(t *testing.T) {
+	// Create a new employee
+	employee, err := NewEmployee("John", "Doe")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if employee != nil {
+		t.Logf("Employee created: %v", employee)
+	}
+}
+
+func TestNewEmployee_WhenFirstNameIsEmpty_ReturnError(t *testing.T) {
+	// Create a new employee
+	employee, err := NewEmployee("", "Doe")
+	if err != nil {
+		t.Logf("Error: %v", err)
+	}
+	if employee == nil {
+		t.Logf("Employee is nil")
+	}
+}
+
+func TestNewEmployee_WhenLastNameIsEmpty_ReturnError(t *testing.T) {
+	// Create a new employee
+	employee, err := NewEmployee("John", "")
+	if err != nil {
+		t.Logf("Error: %v", err)
+	}
+	if employee == nil {
+		t.Logf("Employee is nil")
+	}
+}
+
+func TestEmployee_GetFullName_ReturnValidFullName(t *testing.T) {
+	// Create a new employee
+	employee, err := NewEmployee("John", "Doe")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fullName := employee.GetFullName()
+	//assert the value of fullName
+	if fullName != "John Doe" {
+		t.Fatalf("Expected full name is John Doe, but got %s", fullName)
+	}
+}
