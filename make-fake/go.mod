47188bcb41ff74c689e5aca42e796dc5694f9911
Abdel Raoof <olakara@gmail.com>
2025-02-23 10:42:45 +0400

    using gofakeit library

---
+++ make-fake/go.mod
@@ -0,0 +1,8 @@
+module makefake
+
+go 1.23.6
+
+require (
+	github.com/brianvoe/gofakeit/v7 v7.2.1
+	github.com/google/uuid v1.6.0
+)
---
+++ make-fake/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	fake "github.com/brianvoe/gofakeit/v7"
+	"github.com/google/uuid"
+	"time"
+)
+
+type Person struct {
+	Id          uuid.UUID
+	FirstName   string
+	LastName    string
+	Gender      string
+	DateOfBirth time.Time
+}
+
+func NewPerson() (p *Person, err error) {
+	return &Person{
+		Id:          uuid.New(),
+		FirstName:   fake.FirstName(),
+		LastName:    fake.LastName(),
+		DateOfBirth: fake.Date(),
+	}, nil
+}
+
+func main() {
+
+	name := fake.FirstName()
+	fullName := fake.Person().LastName
+	product := fake.Product()
+
+	fmt.Println("Name: ", name)
+	fmt.Println("Full Name: ", fullName)
+	fmt.Println("Product: ", product)
+
+	person, _ := NewPerson()
+	fmt.Println("Person: ", person)
+}
