135d0ee013e170b1448bd19ca46ae7f410aa2d33
Abdel Raoof <olakara@gmail.com>
2025-03-07 14:53:45 +0400

    implementing factory method

---
+++ patterns/creational/factory/reservation.go
@@ -0,0 +1,19 @@
+﻿package factory
+
+type Reservation interface {
+	GetReservationDate() string
+	CalculateCancellationFee() float64
+	Cancel()
+	GetStatus() string
+}
+
+func NewReservation(vertical, reservationDate string) Reservation {
+	switch vertical {
+	case "flight":
+		return FlightReservationImpl{reservationDate: reservationDate}
+	case "hotel":
+		return HotelReservationImpl{reservationDate: reservationDate}
+	default:
+		return nil
+	}
+}
