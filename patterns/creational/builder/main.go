0e0721759acad61ed0826978672ece0acef5bf11
Abdel Raoof <olakara@gmail.com>
2025-03-07 15:38:58 +0400

    implementing builder method

---
+++ patterns/creational/builder/main.go
@@ -0,0 +1,12 @@
+﻿package main
+
+import (
+	reservation "builder/reservation"
+	"fmt"
+)
+
+func main() {
+	builder := reservation.NewReservationBuilder()
+	flightReservation := builder.SetVertical("flight").SetReservationDate("2021-01-01").Build()
+	fmt.Println("flightReservation:", flightReservation)
+}
