7137b939f4de5dd397b722fc6c588f4dd79972a8
Abdel Raoof <olakara@gmail.com>
2025-05-14 22:14:01 +0400

    wildcard route patterns and getting values

--- web-server/main.go
+++ web-server/main.go
@@ -23,12 +23,20 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Snippet Create"))
 }
 
+func handleProductsItem(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)	
+	log.Println("Received request for:", r.URL.Path)
+	log.Println("Product Item ID:", r.PathValue("itemID"))
+	w.Write([]byte("Product Item"))
+}
+
 func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/{$}", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/products/item/{itemID}", handleProductsItem)
 
 	log.Println("Starting server on :8080")
 	err := http.ListenAndServe(":8080", mux)
